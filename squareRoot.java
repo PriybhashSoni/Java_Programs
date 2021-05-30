e5a8710d1b6a944596a1f431cae55088a3f762a6
PriybhashSoni <77826736+PriybhashSoni@users.noreply.github.com>
2021-05-30 12:33:33 +0530

    Add files via upload

---
+++ squareRoot.java
@@ -0,0 +1,13 @@
+package Number;
+
+import java.util.Scanner;
+
+public class squareRoot {
+    public static void main(String[] args) {
+        int n;
+        Scanner s=new Scanner(System.in);
+        System.out.println("enter number");
+        n=s.nextInt();
+        System.out.println(Math.sqrt(n));
+    }
+}
