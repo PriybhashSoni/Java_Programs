b6d3ac68947fc611390406530a50a3e63a96756c
PriybhashSoni <77826736+PriybhashSoni@users.noreply.github.com>
2021-06-01 09:32:52 +0530

    Add files via upload

---
+++ tignometry.java
@@ -0,0 +1,9 @@
+package Mathematics;
+
+public class tignometry {
+    public static void main(String[] args) {
+        System.out.println("value of cos(0) is "+Math.cos(0));
+        System.out.println("value of sin(0) is "+Math.sin(0));
+        System.out.println("value of tan(0) is "+Math.tan(0));
+    }
+}
