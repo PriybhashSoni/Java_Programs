24e55f36d2ad3e92c50716e33f6aee58e9882f46
PriybhashSoni <77826736+PriybhashSoni@users.noreply.github.com>
2021-06-05 15:46:26 +0530

    Create constructorex1.java

---
+++ constructorex1.java
@@ -0,0 +1,29 @@
+class student {
+    int id;
+    String name;
+    int age;
+
+    student(int i, String n)
+    {
+        id = i;
+        name = n;
+    }
+    student(int i, String n, int a)
+    {
+        id = i;
+        name = n;
+        age = a;
+    }
+    void display()
+    {
+        System.out.println(id + " " + name + " " + age);
+    }
+    public static void main(String args[])
+    {
+        student s1 = new student(111,"Rahul");
+        student s2 = new student(132,"Karan",25);
+
+        s1.display();
+        s2.display();
+    }
+}
