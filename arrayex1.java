a0ccc6a0faf5c24a2b49ac585d5a4f2cfe47cfe8
PriybhashSoni <77826736+PriybhashSoni@users.noreply.github.com>
2021-06-07 14:29:39 +0530

    Add files via upload

---
+++ arrayex1.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class arrayex1 {
+    public static void main(String[] args) {
+        String[] cars={"bmw","maruti","volvo","audi"};
+        //cars[1]="audi";
+        // for(int i=0;i<cars.length;i++)
+        // {
+        //     System.out.println(cars[i]);
+        // }
+        // System.out.println(cars[1]);
+        // System.out.println(cars.length);
+        // for(String i:cars)
+        // {
+        //     System.out.println(i);
+        // }
+    //multidimensional array
+        int[][] mynumber={{1,2,3},{4,5,6}};
+        int x=mynumber[2][2];
+        System.out.println(x);
+    }
+
+}
