59693b2c0c647394fcd0005e96670eb4a680b314
PriybhashSoni <77826736+PriybhashSoni@users.noreply.github.com>
2021-06-11 09:28:39 +0530

    Add files via upload

---
+++ sumoffirstNevennumbers.java
@@ -0,0 +1,14 @@
+package Mathematics;
+
+public class sumoffirstNevennumbers {
+    public static void main(String[] args) {
+        
+        int sum=0;
+        int n=4;
+        for(int i=0;i<n;i++)
+        {
+            sum=2*i;
+        }
+        System.out.println("sum of n first enen number is "+sum);
+    }
+}
