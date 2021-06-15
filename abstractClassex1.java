5907d55b14503140bac57061ae3499425aa19549
PriybhashSoni <77826736+PriybhashSoni@users.noreply.github.com>
2021-06-15 09:33:50 +0530

    Add files via upload

---
+++ abstractClassex1.java
@@ -0,0 +1,30 @@
+package learningEX;
+
+import javax.swing.AbstractAction;
+
+public abstract class abstractClassex1 {
+    public String fname="John";
+    public int age=23;
+    public abstract void study();
+}
+    class student extends abstractClassex1
+    {
+        public int GraduationYear=2018;
+        public void study()
+        {
+            System.out.println("Study all along day");
+
+        }
+    }
+
+    class second{ 
+
+    public static void main(String[] args) {
+     student st=new student();
+        System.out.println("name is "+st.fname);
+        System.out.println("age is "+st.age);
+        System.out.println("graduation year is "+st.GraduationYear);
+            st.study();
+    }
+    }
+
