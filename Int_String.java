7cba4909cebe740e9dc924f64ddbc0b680b90639
PriybhashSoni <77826736+PriybhashSoni@users.noreply.github.com>
2021-06-28 19:32:57 +1100

    Create Int_String.java

---
+++ Int_String.java
@@ -0,0 +1,11 @@
+public class stringint {
+    public static void main(String args[])
+    {
+        String data="10";
+        System.out.println("The string  is "+data);
+        
+        int num=Integer.parseInt(data);
+        System.out.println("The integer is "+num);
+    } 
+    
+}
