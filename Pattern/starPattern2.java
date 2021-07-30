31f36487add8df11b09d9c24650aebe9b91a8e55
Priybhash Soni    प्रियभाष सोनी <py.priybhashsoni@gmail.com>
2021-07-30 21:14:03 +1100

    Update starPattern2.java

--- Pattern/starPattern2.java
+++ Pattern/starPattern2.java
@@ -5,7 +5,7 @@ public class starPattern2 {
         for(int i=1;i<=5;i++){
             for(int j=1;j<=i;j++)
             {
-                System.out.print("**");
+                System.out.print("*");
             }
                System.out.println();
         }
