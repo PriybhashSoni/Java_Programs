d8596c5053a3d46f9ebd208d4c02c1ad391f13dd
Priybhash Soni    प्रियभाष सोनी <py.priybhashsoni@gmail.com>
2021-08-06 18:21:36 +1100

    Update currencyConverter.java

--- currencyConverter.java
+++ currencyConverter.java
@@ -10,7 +10,7 @@ public class currencyConverter {
  
 		DecimalFormat f = new DecimalFormat("##.##");
  
-		Scanner = new Scanner(System.in);
+		Scanner scc= new Scanner(System.in);
  
 		System.out.println("hi, Welcome to the Currency Converter!");
  
