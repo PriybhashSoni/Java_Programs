d5bc41f1e072c786c7e41e4dbd07f0a7168890d3
Priybhash Soni    प्रियभाष सोनी <py.priybhashsoni@gmail.com>
2021-09-02 14:48:11 +0530

    Update dateex1.java

--- dateex1.java
+++ dateex1.java
@@ -3,6 +3,6 @@ import java.time.LocalDate; // import the LocalDate class
 public class Main {
   public static void main(String[] args) {
     LocalDate myObj = LocalDate.now(); // Create a date object
-    System.out.println(myObj); // Display the current date
+    System.out.println(myObj); // Disply the current date
   }
 }
