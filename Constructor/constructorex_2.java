e4a78e337c3ca440133f67c39dd01927fd3d76df
Priybhash Soni    प्रियभाष सोनी <py.priybhashsoni@gmail.com>
2021-10-24 20:32:04 +1100

    Update constructorex_2.java

--- Constructor/constructorex_2.java
+++ Constructor/constructorex_2.java
@@ -1,5 +1,5 @@
 class Bike1{  
-//creating a default constructor  
+//creating a default  constructor  
 Bike1(){System.out.println("Bike is created");}  
 //main method  
 public static void main(String args[]){  
