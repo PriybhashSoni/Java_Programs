9b318df926da0ddae4f65eda8702f86076b6f84f
Priybhash Soni    प्रियभाष सोनी <py.priybhashsoni@gmail.com>
2021-12-24 14:58:24 +1100

    Update inheritanceex2.java

--- Inheritance/inheritanceex2.java
+++ Inheritance/inheritanceex2.java
@@ -2,7 +2,7 @@ class Animal{
 void eat(){System.out.println("eating...");}  
 }  
 class Dog extends Animal{  
-void bark(){System.out.println("barking...");}  
+void bark(){System.out.println("barking....");}  
 }  
 class TestInheritance{  
 public static void main(String args[]){  
